3d771ce8fddb85989b1a0ed8cf7b814b410906d0
if26-grp2 <if26-grp2@mac01-m102.utt.fr>
2018-10-02 17:48:04 +0200

    Initial commit

---
+++ app/src/main/java/fr/utt/if26/nguyenphuc_tp03/MainActivity.java
@@ -0,0 +1,32 @@
+package fr.utt.if26.nguyenphuc_tp03;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     *
+     * @param savedInstanceState
+     *  NONE
+     */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final TextView tx = (TextView) findViewById(R.id.textview);
+        String message = getResources().getString(R.string.message);
+        tx.setText(message);
+        Button bt = (Button) findViewById(R.id.button1);
+        bt.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                tx.setBackgroundColor(Color.RED);
+            }
+        });
+    }
+}
